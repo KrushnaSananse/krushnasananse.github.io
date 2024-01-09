15601a8f627577b37c3543e07d9eb8f274c42535
KrushnaSananse <132080099+KrushnaSananse@users.noreply.github.com>
2024-01-09 18:33:10 +0530

    Updates

---
+++ precache-manifest.6ef25bae168f2c091255d2c1bb8e8f6d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f854c881517402395426",
+    "url": "/static/css/main.a2329fed.chunk.css"
+  },
+  {
+    "revision": "f854c881517402395426",
+    "url": "/static/js/main.f854c881.chunk.js"
+  },
+  {
+    "revision": "6eb6405c9d99258624bc",
+    "url": "/static/js/1.6eb6405c.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "d8fb95f8db8521310b23f8a80d6295a5",
+    "url": "/index.html"
+  }
+];
